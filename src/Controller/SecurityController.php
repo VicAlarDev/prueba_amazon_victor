46ea049fd67bc386d2237dfb300ced42ffee62df
Victor Alarcon <vic.alarcondev@gmail.com>
2024-07-20 13:04:59 -0400

    style: added content to landing

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,11 +12,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_landing_page');
+        }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
